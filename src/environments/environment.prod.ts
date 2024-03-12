cf072c47632126db99b9136e0890ee6b6c0c6075
Sabine <jacquessabine8@gmail.com>
2024-03-12 00:59:02 -0400

    module2_3 added docker compose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  //api_url: 'https://q-a-example-loopback-api.herokuapp.com',
+  api_url: 'http://localhost:3000'
 };
